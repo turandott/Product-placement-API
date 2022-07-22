e17546a76f41caa3a579a88e2596d3f6151fd536
turandott <greyvilka@gmail.com>
2022-07-22 12:25:42 +0300

    Add update action to the users controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,7 +1,9 @@
 class Api::V1::UsersController < ApplicationController
+  before_action :set_user, only: %i[show update]
 
   def show
-    render json:  User.find(params[:id])
+    render json: @user
+    # render json:  User.find(params[:id])
   end
 
   def create
@@ -13,8 +15,21 @@ class Api::V1::UsersController < ApplicationController
     end
   end
 
+  def update
+    if @user.update(user_params)
+      render json: @user, status: :ok
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
+  end
+
   private
   def user_params
     params.require(:user).permit(:email, :password)
+  end
+
+  def set_user
+    @user= User.find(params[:id])
+
   end
 end
