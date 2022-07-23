2f8756b70b13bc1cbef2a932afb7d582152d5c8a
turandott <greyvilka@gmail.com>
2022-07-23 17:03:00 +0300

    Add action for product model and tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,10 @@
 Rails.application.routes.draw do
 
-  namespace :api do
-    namespace :v1 do
-      # get 'tokens/create'
-      resources :tokens, only: [:create]
-    end
-  end
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       resources :users, only: %i[show create update destroy]
+      resources :tokens, only: [:create]
+      resources :products, only: %i[show index]
     end
   end
 end
---
+++ test/controllers/api/v1/products_controller_test.rb
@@ -0,0 +1,26 @@
+require 'test_helper'
+
+class Api::V1::ProductsControllerTest < ActionDispatch::IntegrationTest
+
+  setup do
+    @product = products(:one)
+  end
+
+  test "should show product" do
+    get api_v1_product_url(), as: :json
+    assert_response :success
+  end
+
+
+  test "should show product" do
+    get api_v1_product_url(@product), as: :json
+    assert_response :success
+
+    json_response =JSON.parse(self.response.body)
+    assert_equal @product.title, json_response['title']
+
+  end
+
+
+
+end
