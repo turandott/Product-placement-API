b773c8130c3c65bab72e8e78f914fb5453859f09
turandott <greyvilka@gmail.com>
2022-07-23 20:17:24 +0300

    Add relationship to user show

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,7 +3,8 @@ class Api::V1::UsersController < ApplicationController
   before_action :check_owner, only: %i[update destroy]
 
   def show
-    render json: UserSerializer.new(@user).serializable_hash.to_json
+    options = { inlude: [:products] }
+    render json: UserSerializer.new(@user, options).serializable_hash.to_json
     # render json:  User.find(params[:id])
   end
 
--- test/controllers/api/v1/users_controller_test.rb
+++ test/controllers/api/v1/users_controller_test.rb
@@ -10,7 +10,11 @@ class Api::V1::UsersControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
 
     json_response = JSON.parse(self.response.body)
-    assert_equal @user.email, json_response['data'] ['attributes'] ['email']
+    assert_equal @user.email, json_response.dig( :data, :attributes, :email)
+    assert_equal @user.products.first.id.to_s, json_response.dig( :data, :relationships, :products, :data, 0, :id)
+    assert_equal @user.products.first.title, json_response.dig( :included, 0, :attributes, :title)
+
+
   end
 
   test "should create user" do
