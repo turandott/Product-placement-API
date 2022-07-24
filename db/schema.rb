4bc7cd14b7ca1cc26207f0b613378285b9e5ed34
turandott <greyvilka@gmail.com>
2022-07-24 17:03:32 +0300

    Add orders model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_22_173833) do
+ActiveRecord::Schema.define(version: 2022_07_24_135911) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "orders", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.decimal "total"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_orders_on_user_id"
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "title"
@@ -35,5 +43,6 @@ ActiveRecord::Schema.define(version: 2022_07_22_173833) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "orders", "users"
   add_foreign_key "products", "users"
 end
