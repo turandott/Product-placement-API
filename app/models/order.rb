d7db17b7e0d5050d3d722e1abba3983bf0f7b4dd
turandott <greyvilka@gmail.com>
2022-07-24 17:29:40 +0300

    Add dependense between order and product through placement

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,11 @@
 class Order < ApplicationRecord
-  belongs_to :user
   validates :total, numericality: { greater_than_or_equal_to: 0}
   validates :total, presence: true
+
+  belongs_to :user
+  has_many :placements, dependent: :destroy
+  has_many :products, through: :placements
+
+
 
 end
