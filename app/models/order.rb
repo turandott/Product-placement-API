8f5dc35571bf9ed6d5d7a12590ee2f0a5a7b7c79
turandott <greyvilka@gmail.com>
2022-07-25 15:14:35 +0300

    Add create action to orders

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,6 @@
 class Order < ApplicationRecord
+  before_validation :set_total!
+
   validates :total, numericality: { greater_than_or_equal_to: 0}
   validates :total, presence: true
 
@@ -6,6 +8,9 @@ class Order < ApplicationRecord
   has_many :placements, dependent: :destroy
   has_many :products, through: :placements
 
+  def set_total!
+    self.total=products.sum :price
+  end
 
 
 end
